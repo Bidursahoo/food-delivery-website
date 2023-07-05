813c8c5842fef7bd8566aa2bd4eb3946957a638d
Bidursahoo <bibhabsahoobidur@gmail.com>
2023-07-05 11:54:35 +0530

    pt:10 dynamic navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link , useNavigate } from "react-router-dom";
 export default function Navbar(props) {
+  const navigate =useNavigate();
+  function handleLogout(){
+    localStorage.removeItem("authTocken");
+    navigate("/");
+  }
   return (
     <div>
       <nav className=" navbar navbar-expand-lg bg-info rounded ">
@@ -20,23 +25,38 @@ export default function Navbar(props) {
             <span className="navbar-toggler-icon"></span>
           </button>
 
-          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarNav"
+          >
             <ul className="navbar-nav align-items-center">
+            {localStorage.getItem("authTocken") ? (
               <li className="nav-item">
+                <div className="btn bg-white text-danger mx-1 " onClick={handleLogout}>
+                  Logout
+                </div>
+              </li>
+              ) : (
+                <li className="nav-item">
                 <Link className="nav-link active" aria-current="page" to="/">
                   Home
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/login">
-                  Login
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/signup">
-                  SignUp
-                </Link>
-              </li>
+              )}
+              
+              {localStorage.getItem("authTocken") ? (
+                <div className="btn bg-white text-success mx-1">Cart</div>
+              ) : (
+                <div className="d-flex">
+                  <Link className="btn bg-white text-success mx-1" to="/login">
+                    Login
+                  </Link>
+                  <Link className="btn bg-white text-warning mx-1" to="/signup">
+                    SignUp
+                  </Link>
+                </div>
+              )}
+
               <li className="nav-item">
                 <form className="d-flex">
                   <input
@@ -45,7 +65,7 @@ export default function Navbar(props) {
                     placeholder="Search"
                     aria-label="Search"
                     value={props.search}
-                    onChange={(event)=>{
+                    onChange={(event) => {
                       props.setSearch(event.target.value);
                     }}
                   />
